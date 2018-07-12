bd57f0455678b05baa8ab1cd67712b9a2351a003
Azmadeu <doubleworkz@mail.ru>
2018-07-12 21:13:11 +0300

    create data of carts

---
+++ src/data/index.js
@@ -0,0 +1,42 @@
+export const Carts = [
+  {
+    photo: "https://react-shopping-cart-67954.firebaseapp.com/static/media/8552515751438644_1.84f8290e.jpg",
+    model: "Nike Corinthians I shirt",
+    currency: "$",
+    price: {
+      dollars: 10,
+      cents: ".90"
+    },
+    installment: "or 9x of $ 1.21"
+  },
+  {
+    photo: "https://react-shopping-cart-67954.firebaseapp.com/static/media/18644119330491310_1.e24fb843.jpg",
+    model: "Nike Corinthians II shirt",
+    currency: "$",
+    price: {
+      dollars: 10,
+      cents: ".90"
+    },
+    installment: "or 9x of $ 1.21"
+  },
+  {
+    photo: "https://react-shopping-cart-67954.firebaseapp.com/static/media/11854078013954528_1.f339ec85.jpg",
+    model: "Feminina Nike Corinthians I shirt",
+    currency: "$",
+    price: {
+      dollars: 14,
+      cents: ".90"
+    },
+    installment: `or 7x of $ 2.13`
+  },
+  {
+    photo: "https://react-shopping-cart-67954.firebaseapp.com/static/media/876661122392077_1.5875fccb.jpg",
+    model: "Feminina Nike Corinthians II shirt",
+    currency: "$",
+    price: {
+      dollars: 14,
+      cents: ".90"
+    },
+    installment: "or 9x of $ 2.13"
+  }
+];
