aa4f912c9ff186d6ee47b48783d1476e76473e20
Azmadeu <doubleworkz@mail.ru>
2018-07-12 21:15:01 +0300

    add cart-list for App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import banner from './asserts/img/banner.jpg';
-import Product from './Components/CartsList/index';
+import CartsList from './Components/CartsList/index';
 import ControlledPanel from './Components/TogglePanel/index';
+import Sizes from './Components/OrderBy/index';
+import {Carts} from './data/index'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img className="banner" src={banner} />
-        <Product />
-        <ControlledPanel />
+        <img className="banner" src={banner}/>
+        <CartsList Carts={Carts}/>
+        {/*<Sizes /> */}
+        <ControlledPanel/>
         <footer>Coded by Serge Makarov</footer>
       </div>
     );
--- src/Components/CartsList/index.js
+++ src/Components/CartsList/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
-class Product extends Component {
+class CartsList extends Component {
   render() {
     return (
-      <div>
-
+      <div className="container">
+        {
+          this.props.Carts.map(cart => (
+              <div className="cart">
+                <img src={cart.photo}  />
+                <div className="title">
+                  {cart.model}
+                  </div>
+                <div className="price">
+                  <div>
+                    <small>{cart.currency}</small>
+                    <b>{cart.price.dollars + cart.price.cents}</b>
+                  </div>
+                  <div className="installment">
+                    {cart.installment}
+                  </div>
+                </div>
+                <div className="btn">Add to cart</div>
+              </div>
+            )
+          )
+        }
       </div>
     )
   }
 }
 
-export default Product;
+export default CartsList;
