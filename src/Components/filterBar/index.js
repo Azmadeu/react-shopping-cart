4c37bce174e1b0a9b40040af6d7eeb4fe5f8fd3f
Azmadeu <doubleworkz@mail.ru>
2018-07-12 21:14:39 +0300

    create filterBar directory

--- src/Components/filterBar/index.js
+++ src/Components/filterBar/index.js
@@ -1,1 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+class Sizes extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Sizes
