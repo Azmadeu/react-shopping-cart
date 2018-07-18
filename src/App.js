b887055e7df933389c9ef6b6fca518ef65225755
Azmadeu <doubleworkz@mail.ru>
2018-07-18 05:34:28 +0300

    react-redux & redux have been imported

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
-import banner from './asserts/img/banner.jpg';
+import { Provider } from 'react-redux';
+import { store } from './Redux/index';
+import { Carts } from './data/index';
 import CartsList from './Components/CartsList/index';
 import ControlledPanel from './Components/TogglePanel/index';
 import Filter from './Components/filterBar/index';
-import {Carts} from './data/index'
+import banner from './asserts/img/banner.jpg'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <img
           className="banner"
-          src={banner}/>
+          src={banner}
+          alt="banner"
+        />
+        {/*<div className="header">*/}
+          {/*<small className="products-found">*/}
+            {/*<span>*/}
+              {/*"{Carts.length}" Product(s) found.*/}
+            {/*</span>*/}
+          {/*</small>*/}
+        {/*</div>*/}
         <Filter/>
         <CartsList Carts={Carts} />
         <ControlledPanel/>
       </div>
+      </Provider>
     );
   }
 }
