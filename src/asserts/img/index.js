a47c571ea7c338b6e36c33cde20778480e153e35
Azmadeu <doubleworkz@mail.ru>
2018-07-19 20:36:47 +0300

    9 New Products are added

--- src/asserts/img/index.js
+++ src/asserts/img/index.js
@@ -5,7 +5,17 @@ import Skuul from './Skuul.jpg';
 import WineSkuul from './WineSkuul.jpg';
 import BornOnStreet from './BornOnTheStreet.jpg';
 import ShortSleeve from './ShortSleeveT-Shirt.jpg';
-import SphynxTie from './SphynxTieDyeGreyT-Shirt.jpg'
+import SphynxTie from './SphynxTieDyeGreyT-Shirt.jpg';
+import CatTeeTwo from './Cat-Tee-Black-T-shirt(2).jpg';
+import DangerKnife from './DangerKnife.jpg';
+import WhiteDGK from './WhiteDGK.jpg';
+import Tso3D from './Tso3D.jpg';
+import ManTieDie from './ManTieDie.jpg';
+import CrazyMonkey from './CrazyMonkey.jpg';
+import OnTheStreets from './OnTheStreets.jpg';
+import CrazyMonkeyGrey from './CrazyMonkeyGrey.jpg';
+import Tso3DBlack from './Tso3DBlack.jpg';
+
 
 export {
   CatTee,
@@ -15,5 +25,14 @@ export {
   Skuul,
   Sphynx,
   BornOnStreet,
-  WineSkuul
+  WineSkuul,
+  CatTeeTwo,
+  DangerKnife,
+  WhiteDGK,
+  Tso3D,
+  ManTieDie,
+  CrazyMonkey,
+  OnTheStreets,
+  CrazyMonkeyGrey,
+  Tso3DBlack
 }
